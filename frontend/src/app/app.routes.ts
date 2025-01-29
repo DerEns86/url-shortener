bd8832f247706955aca495b93e4494b662f9059a
DerEns86 <v.ens86@yahoo.de>
2025-01-29 08:51:30 +0100

    test with fallback route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { RedirectComponent } from './pages/redirect/redirect.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'redirect/:shortUrlPath', component: RedirectComponent },
+  { path: '**', component: NotFoundComponent },
 ];
