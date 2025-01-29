65b95334cab64e81f39014fa1d76a4b29326fcfa
DerEns86 <v.ens86@yahoo.de>
2025-01-29 08:29:12 +0100

    update corsFilter

--- backend/src/main/java/dev/ens/backend/security/WebConfig.java
+++ backend/src/main/java/dev/ens/backend/security/WebConfig.java
@@ -17,8 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
-        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("*");
+        // config.addAllowedOrigin("http://localhost:8080");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
